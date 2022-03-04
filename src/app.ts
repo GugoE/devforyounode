15a136736b170b8e89c384a6cade276939fda956
gugo <gaegiazaryan@gmail.com>
2022-03-04 16:16:29 +0400

    a

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(function(req, res, next) {
+app.use((req: any, res: any, next: any) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
